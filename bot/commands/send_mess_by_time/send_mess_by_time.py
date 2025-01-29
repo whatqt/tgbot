cc47d26be177e4be001deb1349a2f5566fbd825c
whatqt <trutnev.07@list.ru>
2025-01-29 20:04:28 +0700

    added notification deletion system:   commands/send_mess_by_time/send_mess_by_time.py 	deleted:    commands/send_mess_by_time/test_logic_time.py
    	modified:   ../.gitignore

--- bot/commands/send_mess_by_time/send_mess_by_time.py
+++ bot/commands/send_mess_by_time/send_mess_by_time.py
@@ -16,6 +16,7 @@ from .text import text_info
 from datetime import datetime, timedelta
 from .logic_calculation_time import CountNextNotification
 import asyncio
+from asyncio import CancelledError
 from lessons.week.week import display_the_schedule
 from lessons.score_week import check_week
 from lessons.current_day import CurrentDay
@@ -25,16 +26,20 @@ from lessons.current_day import CurrentDay
 
 load_dotenv()
 router = Router()
-users_use_notification = []
+# users_use_notification = []
+users_use_notification = {}
 
 
 bot = Bot(token=os.getenv('TOKEN_BOT'))
 
+async def keyboard():
+    builder = InlineKeyboardBuilder()
+    await CallbackButton('Удалить уведомление', 'edit', builder)()
+    return builder.as_markup()
 
-@router.message(Command("send_class_by_time"))
 async def send_mess_by_time(
     message: types.Message,
-    command: CommandObject
+    command: CommandObject,
     ):
     if command.args is None:
         await message.reply(
@@ -52,32 +57,64 @@ async def send_mess_by_time(
         time_from_db
     )
     count_next_notification = CountNextNotification(manage_time.time)
-    users_use_notification.append(message.from_user.id)
+    # users_use_notification.append(message.from_user.id)
+    await message.reply(
+        f"Уведомление установлено на {command.args}",
+        reply_markup=await keyboard()
+    )
 
     while True:
-        if message.from_user.id in users_use_notification:
-            time_to_slep = await count_next_notification.count()
-            print(time_to_slep)
-            await asyncio.sleep(time_to_slep)
-            current_day = CurrentDay()
-            day = await current_day.today_day_week()
-            await display_the_schedule(
-                message.from_user.id, 
-                message, 
-                await check_week(
-                    day
-                ),
-                'answer'
-            )   
-            
-            time_to_slep = await count_next_notification.count()
-            print(time_to_slep)
-            await asyncio.sleep(time_to_slep)
+        # if message.from_user.id in users_use_notification:
+        time_to_slep = await count_next_notification.count()
+        print(time_to_slep)
+        await asyncio.sleep(time_to_slep)
+        current_day = CurrentDay()
+        day = await current_day.today_day_week()
+        await display_the_schedule(
+            message.from_user.id, 
+            message, 
+            await check_week(
+                day
+            ),
+            'answer'
+        )   
+        # без этого скрипт после первой отправки уведомления отправит сразу же второе
+        await asyncio.sleep(10) 
+        time_to_slep = await count_next_notification.count()
+        print(time_to_slep)
+        await asyncio.sleep(time_to_slep)
 
-        else:
-            await manage_send_mess_time.delete_time()
-            await message.answer("Отправка уведомлений была остановлена")
+        # else:
+        #     await manage_send_mess_time.delete_time()
+        #     await message.answer("Отправка уведомлений была остановлена")
 
+@router.message(Command("send_class_by_time"))
+async def view_send_class_by_time(
+    message: types.Message,
+    command: CommandObject
+    ):
+    task = asyncio.create_task(
+        send_mess_by_time(
+            message,
+            command,
+        )
+    )
+    users_use_notification[message.from_user.id] = task
+    try:
+        print("Задача запущена")
+        await task
+    except CancelledError:
+        print("Задача отменена")
+        manage_send_mess_time = ManageSendMessTime(
+            message.from_user.id
+        )
+        await manage_send_mess_time.delete_time()
+        print("Данные удалены")
 
-
-
+@router.callback_query(F.data == 'edit')
+async def edit(callback: types.CallbackQuery):
+    task = users_use_notification[callback.from_user.id]
+    task.cancel()
+    print("Задача отменена в callback edit")
+    await callback.message.edit_text("Используйте ещё раз комманду /send_class_by_time")
+    await callback.answer()
