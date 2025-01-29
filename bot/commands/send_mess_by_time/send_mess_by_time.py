ca867a58a9a5e8a70e8434958c4793ea63e77672
whatqt <trutnev.07@list.ru>
2025-01-29 20:47:03 +0700

    added an alert to the user when the notification is called again:   commands/send_mess_by_time/send_mess_by_time.py 	new text:   commands/send_mess_by_time/text.py
    	modified:   ../.gitignore

--- bot/commands/send_mess_by_time/send_mess_by_time.py
+++ bot/commands/send_mess_by_time/send_mess_by_time.py
@@ -20,19 +20,18 @@ from asyncio import CancelledError
 from lessons.week.week import display_the_schedule
 from lessons.score_week import check_week
 from lessons.current_day import CurrentDay
+from sqlalchemy.exc import IntegrityError
 
 
 
 
 load_dotenv()
 router = Router()
-# users_use_notification = []
 users_use_notification = {}
-
 
 bot = Bot(token=os.getenv('TOKEN_BOT'))
 
-async def keyboard():
+async def keyboard_callback_edit():
     builder = InlineKeyboardBuilder()
     await CallbackButton('Удалить уведомление', 'edit', builder)()
     return builder.as_markup()
@@ -49,22 +48,30 @@ async def send_mess_by_time(
     
     manage_time = ManageTime(command.args)
     time_from_db = await manage_time.date()
-
     manage_send_mess_time = ManageSendMessTime(
         message.from_user.id
     )
-    await manage_send_mess_time.insert_time(
-        time_from_db
-    )
+
+    try:
+        await manage_send_mess_time.insert_time(
+            time_from_db
+        )
+
+    except IntegrityError:
+        time_db = await manage_send_mess_time.return_time_by_id()
+        await message.reply(
+            text_info["there_time"].format(time_db),
+            reply_markup=await keyboard_callback_edit()
+        )
+        return
+
     count_next_notification = CountNextNotification(manage_time.time)
-    # users_use_notification.append(message.from_user.id)
     await message.reply(
         f"Уведомление установлено на {command.args}",
-        reply_markup=await keyboard()
+        reply_markup=await keyboard_callback_edit()
     )
 
     while True:
-        # if message.from_user.id in users_use_notification:
         time_to_slep = await count_next_notification.count()
         print(time_to_slep)
         await asyncio.sleep(time_to_slep)
@@ -78,15 +85,10 @@ async def send_mess_by_time(
             ),
             'answer'
         )   
-        # без этого скрипт после первой отправки уведомления отправит сразу же второе
         await asyncio.sleep(10) 
         time_to_slep = await count_next_notification.count()
         print(time_to_slep)
         await asyncio.sleep(time_to_slep)
-
-        # else:
-        #     await manage_send_mess_time.delete_time()
-        #     await message.answer("Отправка уведомлений была остановлена")
 
 @router.message(Command("send_class_by_time"))
 async def view_send_class_by_time(
@@ -99,7 +101,8 @@ async def view_send_class_by_time(
             command,
         )
     )
-    users_use_notification[message.from_user.id] = task
+    if message.from_user.id not in users_use_notification:
+        users_use_notification[message.from_user.id] = task
     try:
         print("Задача запущена")
         await task
@@ -114,7 +117,9 @@ async def view_send_class_by_time(
 @router.callback_query(F.data == 'edit')
 async def edit(callback: types.CallbackQuery):
     task = users_use_notification[callback.from_user.id]
+    print(task)
     task.cancel()
-    print("Задача отменена в callback edit")
-    await callback.message.edit_text("Используйте ещё раз комманду /send_class_by_time")
+    await callback.message.edit_text(
+        "Используйте ещё раз комманду /send_class_by_time"
+    )
     await callback.answer()
